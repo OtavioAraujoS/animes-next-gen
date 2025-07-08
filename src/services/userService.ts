277837d62ec2c71c1dc1e3d8b28c2a024376c435
OtavioAraujoS <otavioaraujo490@gmail.com>
2025-07-08 14:44:47 -0300

    feat: implement UserService class for user authentication with login method

---
+++ src/services/userService.ts
@@ -0,0 +1,24 @@
+import { ApiResponse } from "@/interfaces/ApiResponse";
+import { api as apiService, ApiService, defaultUrl } from "./api";
+import { User, UserLogin } from "@/interfaces/User";
+
+class UserService {
+  constructor(private readonly api: ApiService) {}
+
+  public async login(data: UserLogin): Promise<ApiResponse<User>> {
+    try {
+      const response = await this.api.post(`${defaultUrl}/users`, data);
+      return {
+        data: response as User,
+        success: true,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: String(error),
+      } as ApiResponse<User>;
+    }
+  }
+}
+
+export const userService = new UserService(apiService);
