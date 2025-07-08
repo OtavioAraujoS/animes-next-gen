c3c8cca967ee05e4de0ea7cd8fc3385e54c6e4d2
OtavioAraujoS <otavioaraujo490@gmail.com>
2025-07-08 09:07:06 -0300

    fix: move cz-shortcut-listen attribute to body tag in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning={true} cz-shortcut-listen="true" data-lt-installed="true">
+    <html lang="en" suppressHydrationWarning={true} data-lt-installed="true">
       <body
+        cz-shortcut-listen="true"
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
