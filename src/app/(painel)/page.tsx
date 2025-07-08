d15d5a3f9ca4c869c3f4bed5d08c9c01b39e5c40
OtavioAraujoS <otavioaraujo490@gmail.com>
2025-07-08 13:48:46 -0300

    feat: implement user redirection to login if not authenticated

--- src/app/(painel)/page.tsx
+++ src/app/(painel)/page.tsx
@@ -1,8 +1,26 @@
+"use client";
+import { useEffect } from "react";
+import { useUserStore } from "../hooks/store/user";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-    return (
-        <div>
-            <h1>Home Page</h1>
-            <p>Welcome to the Anime Next Gen application!</p>
-        </div>
-    )
+  const { user } = useUserStore();
+  console.log("User:", user);
+  const router = useRouter()
+
+  const handleRedirect = () => { 
+    router.push("/login");
+  }
+
+  useEffect(() => {
+    if (!user) {
+      handleRedirect();
+    }
+  }, [user]);
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the Anime Next Gen application!</p>
+    </div>
+  );
 }
